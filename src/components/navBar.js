880a172a1237373351d21e92a15416af8e094933
kelseybcrow <kelseybcrow@gmail.com>
2022-04-22 18:13:58 -0600

    code

---
+++ src/components/navBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <Link className='navbar-brand' to='/'>
+                Vidly
+            </Link>
+            <button
+                className='navbar-toggler'
+                type='button'
+                data-toggle='collapse'
+                data-target='#navbarNav'
+                aria-controls='navbarNav'
+                aria-expanded='false'
+                aria-label='Toggle navigation'>
+                <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbarNav'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item active'>
+                        <NavLink className='nav-link' to='#'>
+                            Home <span className='sr-only'>(current)</span>
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='#'>
+                            Features
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='#'>
+                            Pricing
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
